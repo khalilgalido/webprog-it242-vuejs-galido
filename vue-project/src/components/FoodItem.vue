cc9360f4efbb5460efd80dc4b3f9088eb8ae2dbc
Rei Khalil S. Galido <rsgalido@student.apc.edu.ph>
2026-01-13 11:13:58 +0800

    i like pineapples too

--- vue-project/src/components/FoodItem.vue
+++ vue-project/src/components/FoodItem.vue
@@ -9,8 +9,8 @@
 export default {
   data() {
     return {
-      name: 'Apples',
-      message: 'I like apples'
+      name: 'Apples and pineapples',
+      message: 'I like apples and pineapples'
     }
   }
 };
