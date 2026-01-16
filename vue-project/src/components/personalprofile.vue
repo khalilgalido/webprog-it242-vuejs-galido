8b677a6b2061a2a85de39f7c1af1803a59ad3342
Rei Khalil S. Galido <rsgalido@student.apc.edu.ph>
2026-01-16 11:52:18 +0800

    pls

--- vue-project/src/components/personalprofile.vue
+++ vue-project/src/components/personalprofile.vue
@@ -1,28 +1,23 @@
 <template>
   <div class="container">
+    <h1>Khalil's Personal Profile</h1>
+    <p>
+      <b>About Me</b><br>
+      Fun guy, obviously I love the game of basketball, and I'm a car guy.<br><br>
 
-<h1>Khalil's Personal Profile</h1>
-<p>
-    <b>About Me</b><br>
-    Fun guy, obviously I love the game of basketball, and I'm a car guy.<br><br>
+      <b>Education / Achievements</b><br>
+      <b>Course:</b><br>Information Technology<br><br>
 
-    <b>Education / Achievements</b><br>
-    Course:</b><br>Informational Technology<br><br>
+      <b>IT Experience</b><br><br>
 
-    <b>IT Experience</b><br><br>
+      <b>Hobbies & Interests</b><br><br>
 
-    <b>Hobbies & Interests</b><br><br>
+      <b>Goals In Life / Dream</b><br><br>
 
-    <b>Goals In Life / Dream</b><br><br>
-
-    <b>Picture Gallery</b><br>
-    <img src="https://tse3.mm.bing.net/th/id/OIP.czonCC0IfwwC3EVn4piLJgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3"alt="Kirkified YN">
+      <b>Picture Gallery</b><br>
+      <img src="https://tse3.mm.bing.net/th/id/OIP.czonCC0IfwwC3EVn4piLJgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" alt="Kirkified YN">
     </p>
-
-</div>
+  </div>
 </template>
-
-<script>
-</script>
 
 <style src="./css/PersonalProfile.css"></style>
