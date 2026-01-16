4603e4aecb73a59db80e05a02033a828b5c97b31
Rei Khalil S. Galido <rsgalido@student.apc.edu.ph>
2026-01-16 11:42:22 +0800

    typos

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -3,10 +3,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import FoodItemVue from './components/FoodItem.vue'
 import FoodItem2Vue from './components/FoodItem2.vue'
-import PersonalProfile from './components/PersonalProfile.vue'
+import personalprofile from './components/personalprofile.vue'
 const app = createApp(App)
 app.component('food-item', FoodItemVue)
 app.component('food-item2', FoodItem2Vue)
-app.component('personal-profile', PersonalProfile)
+app.component('personal-profile', personalprofile)
 app.mount('#app')
 
