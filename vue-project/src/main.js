3266a92e64cb69d3db2f2eef34e9af2efb66c4ec
Rei Khalil S. Galido <rsgalido@student.apc.edu.ph>
2026-01-16 10:27:01 +0800

    plswrk

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -5,5 +5,5 @@ import FoodItemVue from './components/FoodItem.vue'
 import FoodItem2Vue from './components/FoodItem2.vue'
 const app = createApp(App)
 app.component('food-item', FoodItemVue)
-app.component('food-item', FoodItem2Vue)
+app.component('food-item2', FoodItem2Vue)
 app.mount('#app')
