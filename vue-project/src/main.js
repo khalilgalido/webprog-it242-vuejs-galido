a15a40efc15b39983c4f73ada9e1fda2330c26ee
Rei Khalil S. Galido <rsgalido@student.apc.edu.ph>
2026-01-27 11:15:06 +0800

    Task 3 import

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -4,11 +4,11 @@ import App from './App.vue'
 import FoodItemVue from './components/FoodItem.vue'
 import FoodItem2Vue from './components/FoodItem2.vue'
 import CommentForm from './components/CommentForm.vue'
-import personalprofile from './components/personalprofile.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
 const app = createApp(App)
 app.component('food-item', FoodItemVue)
 app.component('food-item2', FoodItem2Vue)
-app.component('personal-profile', personalprofile)
+app.component('personal-profile', PersonalProfile)
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
 app.mount('#app')
