79f1b4296f3f5eeb270a2db166b3b31b2f886ae2
Rei Khalil S. Galido <rsgalido@student.apc.edu.ph>
2026-01-27 10:51:25 +0800

    task3

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,61 +1,11 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-
-const instruments = ref([])
-
-async function getInstruments() {
-  // We explicitly ask for 'error' here so we can see it
-  const { data, error } = await supabase.from('instruments').select()
-
-  if (error) {
-    console.error('Supabase Error:', error)
-  } else {
-    console.log('Supabase Data:', data)
-    instruments.value = data
-  }
-}
-
-onMounted(() => {
-   getInstruments()
-})
-</script>
-
 <template>
-  <div class="container">
-    <h1>My Instruments</h1>
-    
-    <ul>
-      <li v-for="instrument in instruments" :key="instrument.id">
-        {{ instrument.name }}
-      </li>
-    </ul>
+  <h1>GoodFood</h1>
+  <food-item/>
+  <personalprofile/>
+  <comment-form/>
+  <comment/>
+   
 
-    <p v-if="instruments.length === 0">Loading instruments...</p>
-  </div>
 </template>
 
-<style>
-/* Basic styling to ensure text is visible */
-.container {
-  padding: 2rem;
-  font-family: sans-serif;
-  color: #ffffff; /* Dark grey text */
-  background-color: #fff; /* White background */
-}
-
-h1 {
-  color: #ffffff;
-  margin-bottom: 1rem;
-}
-
-ul {
-  list-style-type: disc;
-  padding-left: 20px;
-}
-
-li {
-  font-size: 1.2rem;
-  margin-bottom: 0.5rem;
-}
-</style>
+<script></script>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -3,10 +3,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import FoodItemVue from './components/FoodItem.vue'
 import FoodItem2Vue from './components/FoodItem2.vue'
+import Country from './components/Country.vue'
+import CommentForm from './components/CommentForm.vue'
 import personalprofile from './components/personalprofile.vue'
 const app = createApp(App)
 app.component('food-item', FoodItemVue)
 app.component('food-item2', FoodItem2Vue)
 app.component('personal-profile', personalprofile)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
 app.mount('#app')
+
 
