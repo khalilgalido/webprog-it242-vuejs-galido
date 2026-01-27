bf0f05c90bd7a6d58bceda1e93054a9f397ddfbf
Rei Khalil S. Galido <rsgalido@student.apc.edu.ph>
2026-01-27 11:24:34 +0800

    hopefully

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,9 +1,28 @@
 <template>
   <h1>GoodFood</h1>
-  <comment-form/>
-  <comment/>
-  <PersonalProfile/>
-
+  <comment-form />
+  <comment />
+  <PersonalProfile />
 </template>
 
-<script></script>
+<script>
+// 1. IMPORT the component
+// (Make sure the path './components/...' matches your actual folder structure)
+import PersonalProfile from './components/PersonalProfile.vue'
+import CommentForm from './components/CommentForm.vue' // Assuming you have this
+import Comment from './components/Comment.vue'         // Assuming you have this
+
+export default {
+  name: 'App',
+  // 2. REGISTER the component so the template can use it
+  components: {
+    PersonalProfile,
+    CommentForm,
+    Comment
+  }
+}
+</script>
+
+<style>
+/* Your global styles */
+</style>
